8429345b998cc3e24c3078b45e36149a34602ab3
Adriana Cividini <121236952+AdrianaCividini@users.noreply.github.com>
2024-03-11 16:07:25 +0100

    Added bootstrap.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>React App⭐️</p>
+        <h1>React App⭐️</h1>
+        <footer>
+          This project was coded by Adriana Cividini and is{" "}
+          <a href="https://github.com/AdrianaCividini/revisao-weather-app">
+            open-sourced in GitHub.
+          </a>
+        </footer>
       </header>
     </div>
   );
