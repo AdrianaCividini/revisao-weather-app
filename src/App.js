84d8f1cd02512805c1d9f1e25661f9cdbcd24018
Adriana Cividini <121236952+AdrianaCividini@users.noreply.github.com>
2024-03-19 17:00:00 +0100

    added the Axios and useState.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
@@ -6,6 +7,7 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <h1>React App⭐️</h1>
+        <Weather defaultCity="Kyoto" />
         <footer>
           This project was coded by Adriana Cividini and is{" "}
           <a href="https://github.com/AdrianaCividini/revisao-weather-app">
---
+++ src/Weather.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "./Weather.css";
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+      date: "tuesday",
+      iconUrl:
+        "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png",
+      wind: response.data.wind.speed,
+      city: response.data.city,
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="text"
+                placeholder="Type a city..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAVVJREFUaN7tmckNgzAQRV1CSqAEl+ASKIFjjpTgEiiBElICJeTKjXRAB5MZyUgOyoKNx8hkLL1LNv6D8RoFAKpklAiIgAiIgAiIwMsLGxs8rjXSI3cE3nB379cqYdstgIEsMn8I/Qn6vD1UAAMYZAoMvoa+r7ML4EWbncHXNNkEGMLvkggSYAy/ULMJ4I9XEZ0VIjp3xSVwYw6/cEsu4EYcyIhOLdBnFuhTC3DXPs3SgzeLD3TdcRw1Yj10sABz+VBgsxrpLgSG7RB4Qxcq0DKGp6AtMnsBB3e34QttiIBlEjAuPEQwhQh0HHff1fgcKQBHCtCA0GCIKja8/yToKeYsoaXuzYY6D6HL1YlpWGwSBvfR3MPo7rr/geWeyHonYJkw3EsJqxjafyzmil9On2JDU/yW8hSb+lMcq5ziYCvB0eJ0+NFiMYe78v+ACIiACIiACJTEEyDCTi8sMWUSAAAAAElFTkSuQmCC"
+                alt="Sunny day"
+                className="float-left"
+              />
+            </div>
+            <div className="float-left">
+              <span className="temperature">
+                {Math.round(weatherData.temperature)}
+              </span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+
+          <div className="col-6">
+            <ul>
+              <li></li>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} Km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    const apiKey = "be4f04372f126ocaa2t8a5df316fc3ab";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
+}
